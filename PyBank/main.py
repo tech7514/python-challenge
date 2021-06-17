f947f06b1c1a247517fa9fed9bab1bb777fdad5a
tech7514 <80295938+tech7514@users.noreply.github.com>
2021-06-16 21:17:17 -0400

    python_challenge
    set up folder and files

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+#analyzes the records to calculate each of the following
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#Calculate the changes in "Profit/Losses" over the entire period, 
+# then find the average of those change
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
